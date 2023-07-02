ed017856e41a68dd684068b893f2d73d6ad4e0e2
destruc123 <110302857+destruc123@users.noreply.github.com>
2023-07-02 17:32:48 -0500

    first commit

---
+++ sobre mi.js
@@ -0,0 +1,12 @@
+const wallace={
+    nombre: 'wallace',
+    edad: 25,
+    estado_desarrollador: "independiente",
+    cumpleaños: "frebruary 2 1998",
+    libro_favorite:{
+        titulo: "padre rico padre pobre",
+        autor: "Robert Toru Kiyosaki ",
+        fecha: "1997",
+        URL: "https://www.amazon.com.mx/s?k=padre+rico+padre+pobre&i=stripbooks&ref=nb_sb_ss_ts-doa-p_1_6"
+    }
+}
